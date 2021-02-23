0853ba94187044b666328dc767acd2047dbfb144
helenesolhaug <586613@stud.hvl.no>
2021-02-23 15:11:50 +0100

    testmerge

--- src/no/hvl/dat110/messages/CreateTopicMsg.java
+++ src/no/hvl/dat110/messages/CreateTopicMsg.java
@@ -2,11 +2,14 @@ package no.hvl.dat110.messages;
 
 public class CreateTopicMsg extends Message {
 	
-	// message sent from client to create topic on the broker
+	// message sent from client (publisher) to create topic on the broker
+	
 	
 	// TODO: 
 	// Implement object variables - a topic is required
+	MessageType CREATETOPIC;
 	
 	// Constructor, get/set-methods, and toString method
+	
     // as described in the project text	
 }
